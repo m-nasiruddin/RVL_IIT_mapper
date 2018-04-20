4cb6450ee3f1ccf2778726405f2d7836c2a6c423
Mohammad Nasiruddin <mohammad.nasiruddin@gmail.com>
2018-04-20 10:53:00 +0200

    uploaded

---
+++ RVL_IIT_mapper.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 18:21:15 2018
+
+@author: osboxes
+"""
+
+import xml.etree.ElementTree as et
+import re
+
+source = open('data/input/val.txt', 'r')
+corpus = source.readlines()
+rvlcdip_list = []
+for line in corpus:
+    rvlcdip_list.append(re.findall(r'[+_a-z0-9\.-]*.tif', line))
+
+bt_ot_dict = {}
+tree = et.parse('data/input/iitcdip.a.a.xml')
+root = tree.getroot()
+for ltdlwocr in root.iter('LTDLWOCR'):
+    for bt, ot in zip(ltdlwocr.iter('bt'), ltdlwocr.iter('ot')):
+        new_bt = bt.text.split('/')[0]
+        if new_bt in str(rvlcdip_list):
+            print(new_bt)
+            bt_ot_dict.update({new_bt : ot.text})
+
+for key, value in bt_ot_dict.items():
+    print(key, value)
